2135c04ad9f3fba7c724bf958983923ff86c9fe5
favoritegithub <favoritemail.sooyoung@gmail.com>
2020-05-22 16:43:41 +0900

    Handing Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+    console.log(req);
+    res.send("Hello From Home!");
+}
+
+function handleProfile(req, res) {
+    res.send("Hello From Profile!");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
