ecb235574326e988aa4b80367f526bcdfda24cc6
favoritegithub <favoritemail.sooyoung@gmail.com>
2020-05-22 17:35:00 +0900

    Middleware part 2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -7,20 +12,15 @@ const handleListening = () => {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
-const handleHome = (req, res) => {
-    res.send("Hello From Home!");
-}
+const handleHome = (req, res) => res.send("Hello From Home!");
 
-const handleProfile = (req, res) => {
-    res.send("Hello From Profile!");
-}
+const handleProfile = (req, res) => res.send("Hello From Profile!");
 
-const betweenHome = (req, res, next) => {
-    console.log("I'm Between");
-    next();
-}
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(logger("dev"));
+app.use(helmet());
 
 app.get("/", handleHome);
 
